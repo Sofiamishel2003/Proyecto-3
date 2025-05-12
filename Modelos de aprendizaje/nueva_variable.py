030b1bf4d8bcf2d029035b473129ac6fd976528e
Fabiola Contreras <fcontrerascol@gmail.com>
2025-05-12 15:06:50 -0600

    feat: generate, save and use sample for data

--- Modelos de aprendizaje/nueva_variable.py
+++ Modelos de aprendizaje/nueva_variable.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
 # Cargar datos
-matrimonios_df = pd.read_csv("matrimonios_limpio.csv")
-divorcios_df = pd.read_csv("divorcios_limpio.csv")
+matrimonios_df = pd.read_csv("Datos\matrimonios_limpio.csv")
+divorcios_df = pd.read_csv("Datos\divorcios_limpio.csv")
 
 # Crear una firma con variables comunes
 matrimonios_df['firma'] = (
-    matrimonios_df['Ano Ocurrencia'].astype(str) + '_' +
     matrimonios_df['Departamento de Ocurrencia'].astype(str) + '_' +
     matrimonios_df['Edad del Hombre'].astype(str) + '_' +
     matrimonios_df['Edad de la Mujer'].astype(str) + '_' +
@@ -15,7 +14,6 @@ matrimonios_df['firma'] = (
 )
 
 divorcios_df['firma'] = (
-    divorcios_df['Ano Ocurrencia'].astype(str) + '_' +
     divorcios_df['Departamento de Ocurrencia'].astype(str) + '_' +
     divorcios_df['Edad del Hombre'].astype(str) + '_' +
     divorcios_df['Edad de la Mujer'].astype(str) + '_' +
@@ -33,10 +31,33 @@ matrimonios_df['divorcio'] = matrimonios_df['firma'].apply(lambda x: 1 if x in f
 matrimonios_df = matrimonios_df.drop(columns=['firma'])
 
 # Guardar
-matrimonios_df.to_csv("dataset_modelo.csv", index=False)
+matrimonios_df.to_csv("Datos\dataset_modelo.csv", index=False)
 
 # Total de registros
 print("Total:", len(matrimonios_df))
 
 # Conteo de divorcios == 0 y == 1
 print(matrimonios_df['divorcio'].value_counts())
+
+# Obtener una muestra con distribución equitativa entre clases
+# Separar las clases
+divorciados = matrimonios_df[matrimonios_df['divorcio'] == 1]
+no_divorciados = matrimonios_df[matrimonios_df['divorcio'] == 0]
+
+# Muestrear aleatoriamente la misma cantidad que los divorciados
+no_divorciados_sample = no_divorciados.sample(n=len(divorciados), random_state=42)
+
+# Unir las dos clases
+df_balanceado = pd.concat([divorciados, no_divorciados_sample])
+
+# Mezclar el orden de las filas
+df_balanceado = df_balanceado.sample(frac=1, random_state=42).reset_index(drop=True)
+
+# Guardar
+df_balanceado.to_csv("Datos\dataset_muestra.csv", index=False)
+
+# Total de registros
+print("Total:", len(matrimonios_df))
+
+# Verificar distribución
+print(df_balanceado['divorcio'].value_counts())
